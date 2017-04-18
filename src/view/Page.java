deb68d06e36569f1fb36f08e47e5855808aa2030
Batbara <alien.katte@gmail.com>
2017-04-18 12:31:47 +0300

    Edited paging

---
+++ src/view/Page.java
@@ -0,0 +1,51 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Batbara on 18.04.2017.
+ */
+public class Page {
+    private int RECORDS_ON_PAGE = 10;
+    private List<TableRecord> pageContent;
+    public Page(){
+        pageContent = new ArrayList<>();
+    }
+    public boolean add(TableRecord recordToAdd){
+        if(pageContent.size()!=RECORDS_ON_PAGE) {
+            pageContent.add(recordToAdd);
+            return true;
+        }
+        return false;
+
+    }
+
+    public List<TableRecord> getPageContent() {
+        return pageContent;
+    }
+    public int getNumberOfPageRecords(){
+        return pageContent.size();
+    }
+
+    public void removeRecord(TableRecord recordToRemove){
+        for (TableRecord record : pageContent) {
+            if (record.equals(recordToRemove)) {
+                pageContent.remove(record);
+                return;
+            }
+        }
+    }
+
+    public List<List<String>> getListOfRows(){
+        List<List<String>> listOfRows =  new ArrayList<>();
+        for (TableRecord recordToConvert : pageContent){
+            listOfRows.add(recordToConvert.getRecordData());
+        }
+        return listOfRows;
+    }
+    public boolean isEmpty(){
+        return pageContent.isEmpty();
+    }
+
+}
